9af3fdbae6057401bb0c3b860d8d786a82ae1aa8
João Mello <joao.mello@myfreecomm.com.br>
2017-07-13 18:18:47 -0300

    Add specs for profile_by_email method

--- spec/passaporte_web/identity_spec.rb
+++ spec/passaporte_web/identity_spec.rb
@@ -219,6 +219,46 @@ describe PassaporteWeb::Identity do
     end
   end
 
+  # DEPRECATED
+  describe ".profile_by_email", :vcr => true do
+    it "should find the requested profile by email" do
+      identity = described_class.profile_by_email("teste@teste.com")
+      expect(identity).to be_instance_of(described_class)
+      expect(identity.uuid).to eq('5e32f927-c4ab-404e-a91c-b2abc05afb56')
+      expect(identity).to be_persisted
+      expect(identity.email).to eq('teste@teste.com')
+      expect(identity.update_info_url).to eq('https://sandbox.app.passaporteweb.com.br/accounts/api/identities/5e32f927-c4ab-404e-a91c-b2abc05afb56/')
+      expect(identity.accounts.size).to eq(8)
+      expect(identity.accounts.map { |a| a['expiration'] }.uniq).to eq([nil])
+      expect(identity.accounts.map { |a| a['services'] }.flatten.uniq.map { |s| s['slug'] rescue nil }.sort).to eq([nil])
+      expect(identity.services.size).to eq(1)
+      expect(identity.services.keys).to eq(['identity_client'])
+    end
+    it "should find the requested profile by email, including expired accounts" do
+      identity = described_class.profile_by_email("teste@teste.com")
+      expect(identity.accounts.size).to eq(8)
+      expect(identity.accounts.map { |a| a['expiration'] }.uniq).to eq([nil])
+      expect(identity.accounts.map { |a| a['services'] }.flatten.uniq.map { |s| s['slug'] rescue nil }.sort).to eq([nil])
+      expect(identity.services.size).to eq(1)
+      expect(identity.services.keys).to eq(['identity_client'])
+    end
+    it "should find the requested profile by email, including other services" do
+      identity = described_class.profile_by_email("teste@teste.com")
+      expect(identity.accounts.size).to eq(8)
+      expect(identity.accounts.map { |a| a['expiration'] }.uniq).to eq([nil])
+    end
+    it "should find the requested profile by email, including other services and expired accounts" do
+      identity = described_class.profile_by_email("teste@teste.com")
+      expect(identity.accounts.size).to eq(8)
+      expect(identity.accounts.map { |a| a['expiration'] }.uniq).to eq([nil])
+    end
+    it "should raise an error if no profiles exist with that email" do
+      expect {
+        described_class.find("invalid@email.com")
+      }.to raise_error(RestClient::ResourceNotFound, '404 Resource Not Found')
+    end
+  end
+
   describe ".authenticate", vcr: true do
     it "should return an instance of Identity if the password is correct for the given email" do
       identity = described_class.authenticate('teste@teste.com', '123456')
